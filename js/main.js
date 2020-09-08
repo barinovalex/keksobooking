dde19a046511fc17980de57f69a2143a87480490
barinovalex <67120204+barinovalex@users.noreply.github.com>
2020-09-08 15:44:04 +0300

    Module3 Task3 done

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditio
 var titles = ['Помещение 1', 'Уютная квартира', 'Двухэтажный коттедж', 'Апартаменты в новом билдинге', 'Номер в отеле', 'Уютно и не дорого', 'Все включено', 'Помещение 3'];
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
 function shuffle(array) {
   for (var i = array.length - 1; i > 0; i--) {
@@ -37,8 +38,8 @@ var generateData = function (count) {
           address: "600, 350",
           price: Math.floor(Math.random() * 10000),
           type: getRandomArrElement(types),
-          rooms: Math.floor(Math.random() * 5),
-          guests: Math.floor(Math.random() * 10),
+          rooms: Math.floor(Math.random() * 5) + 1,
+          guests: Math.floor(Math.random() * 10) + 1,
           checkin: getRandomArrElement(times),
           checkout: getRandomArrElement(times),
           features: getRandomArr(features),
@@ -71,7 +72,42 @@ var cards = generateData(8);
 
 for (i = 0;i < cards.length;i++) {
   fragment.appendChild(renderPin(cards[i]));
-};
+}
 
 document.querySelector('.map__pins').appendChild(fragment);
 document.querySelector('.map').classList.remove('map--faded');
+
+var map_card = cardTemplate.cloneNode(true);
+
+map_card.querySelector('img').setAttribute('src', cards[0].author.avatar);
+map_card.querySelector('.popup__title').textContent = cards[0].offer.title;
+map_card.querySelector('.popup__text--address').textContent = cards[0].offer.address;
+map_card.querySelector('.popup__text--price').innerHTML = cards[0].offer.price + '&#x20bd;<span>/ночь</span>';
+map_card.querySelector('.popup__type').textContent = cards[0].offer.type;
+map_card.querySelector('.popup__text--capacity').textContent = cards[0].offer.rooms + ' комнаты для ' + cards[0].offer.guests + ' гостей';
+map_card.querySelector('.popup__text--time').textContent = 'Заезд после ' + cards[0].offer.checkin + ', выезд до ' + cards[0].offer.checkout;
+map_card.querySelector('.popup__description').textContent = cards[0].offer.description;
+var mapCardFeatures = map_card.querySelector('.popup__features');
+while (mapCardFeatures.firstChild) {
+  mapCardFeatures.firstChild.remove();
+}
+for (i = 0;i < cards[0].offer.features.length; i++) {
+  var li = document.createElement('li');
+  li.className = "popup__feature popup__feature--" + cards[0].offer.features[i];
+  mapCardFeatures.appendChild(li);
+}
+var mapCardPhotos = map_card.querySelector('.popup__photos');
+while (mapCardPhotos.firstChild) {
+  mapCardPhotos.firstChild.remove();
+}
+for (i = 0;i < cards[0].offer.photos.length; i++) {
+  var img = document.createElement('img');
+  img.className = "popup__photo";
+  img.width = 45;
+  img.height = 40;
+  img.src = cards[0].offer.photos[i];
+  img.alt = 'Фотография жилья';
+  mapCardPhotos.appendChild(img);
+}
+
+document.querySelector('.map__filters-container').before(map_card);
